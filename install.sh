098422e6dbfa274fd49e091c7350262eebbdae22
bonAngeLOL <angellopezcue@gmail.com>
2020-12-08 23:31:20 -0600

    Installer messages changed

--- install.sh
+++ install.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-ErrorM="An error happened, check the install instructions at echo https://github.com/bonanAngeLOL/pySocket-testajxo"
-echo -e "\n\nInstalling ... proyect from GitHub repository\n"
+ErrorM="\n\nAn error happened, check the install instructions at echo https://github.com/bonanAngeLOL/pySocket-testajxo\n\n"
+echo -e "\n\nInstalling ... proyect from GitHub repository\n\n"
 git clone https://github.com/bonanAngeLOL/pySocket-testajxo.git
 if [ $? -eq 0 ]; then
-    echo "Repository cloned"
+    echo "\n\n!!!!!!Repository cloned\n\n"
 else
-    echo $ErrorM
-    echo "Could not clone repository"
+    echo -e $ErrorM
+    echo -e "\n\n!!!!Could not clone repository\n\n"
     exit 1
 fi
 cd pySocket-testajxo
 python3 -m venv venv
 if [ $? -eq 0 ]; then
-    echo "Python VENV created"
+    echo -e "\n\n!!!!! Python VENV created\n\n"
 else
-    echo $ErrorM
-    echo "Could not create VENV, check your python installation"
+    echo -e $ErrorM
+    echo -e "\n\n!!!!! Could not create VENV, check your python installation\n\n"
     exit 1
 fi
 source venv/bin/activate
 if [ $? -eq 0 ]; then
-    echo "Executing now from VENV"
+    echo -e "\n\n!!!!!! Executing now from VENV \n\n "
 else
-    echo $ErrorM
-    echo "VENV python installation not found"
+    echo -e $ErrorM
+    echo -e "\n\n!!!!!VENV python installation not found\n\n"
     exit 1
 fi
 pip install -r requirements.txt
 if [ $? -eq 0 ]; then
-    echo "All the requirements successfully installed"
+    echo -e "\n\n!!!!!All the requirements successfully installed\n\n"
 else
-    echo $ErrorM
-    echo "PIP error installing requirements"
+    echo -e $ErrorM
+    echo -e "\n\n !!!!! PIP error installing requirements\n\n"
     exit 1
 fi
+echo -e "\n\n!!!Project installed successfully!!!\n\n"
